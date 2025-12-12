41c405eefb33a6357eb5429678b9d2798480d702
Felipe Aguirre <felipeaguirreee@gmail.com>
2025-12-12 13:20:04 -0300

    creando swagger

--- practicapp/src/main/java/com/evaluacion/practicapp/controllers/PracticaController.java
+++ practicapp/src/main/java/com/evaluacion/practicapp/controllers/PracticaController.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 
+
 @RestController
 @RequestMapping("/api")
 public class PracticaController {
